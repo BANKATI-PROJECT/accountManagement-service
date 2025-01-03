3ca7eb37b6fa88e1eedf857c888f9249b0b6295c
OuyoussMeryem <ouyss.meryem@gmail.com>
2025-01-03 21:12:21 +0100

    ajouter la dependence de openfeign liee un clien a son portfeuille

--- src/main/java/ma/ensa/accountManagement_service/controllers/ClientController.java
+++ src/main/java/ma/ensa/accountManagement_service/controllers/ClientController.java
@@ -1,4 +1,22 @@
 package ma.ensa.accountManagement_service.controllers;
 
+import ma.ensa.accountManagement_service.model.Portefeuille;
+import ma.ensa.accountManagement_service.feign.PortefeuilleClientFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
 public class ClientController {
+    @Autowired
+    private PortefeuilleClientFeign portefeuilleClient;
+
+    @GetMapping("/portefeuille/{clientId}")
+    public ResponseEntity<Portefeuille> testFeign(@PathVariable Long clientId) {
+        return ResponseEntity.ok(portefeuilleClient.getPortefeuilleByClientId(clientId));
+    }
 }
---
+++ src/main/java/ma/ensa/accountManagement_service/feign/PortefeuilleClientFeign.java
@@ -0,0 +1,18 @@
+package ma.ensa.accountManagement_service.feign;
+
+import ma.ensa.accountManagement_service.model.Portefeuille;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "portefeuille-service")
+public interface PortefeuilleClientFeign {
+
+    @GetMapping("api/portefeuilles/getByClientId/{clientId}")
+    Portefeuille getPortefeuilleByClientId(@PathVariable("clientId") Long clientId);
+
+    @PutMapping("api/portefeuilles/updateByClientId/{clientId}")
+    Portefeuille updatePortefeuille(@PathVariable("clientId") Long clientId, @RequestBody Portefeuille portefeuille);
+}
